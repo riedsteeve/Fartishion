01f9cbb78beeb80dfa819f6433b6c696100bd936
Steeven HOUNKPE-SAGBO <e.hounkpesagbo@ecoles-epsi.net>
2026-06-18 19:39:20 +0200

    Mise en place de la section statistics

---
+++ frontend/src/Components/Composables/fartishionEnChiffre.tsx
@@ -0,0 +1,19 @@
+const stats = [
+    { id: 1, chiffre: '10k+', description: "Partitions disponibles" },
+    { id: 2, chiffre: '45k+', description: "Musiciens inscrits" },
+    { id: 3, chiffre: '120k+', description: "Compositeurs référencés" },
+    { id: 4, chiffre: '98%', description: "Satisafction utilisateurs" },
+]
+
+
+const FartishionEnChiffre = () => {
+    return (
+        <div className="mt-20 bg-purple-600 rounded-4xl p-7">
+
+            <h2 className="text-3xl text-center font-pt-serif" >Fartshion en chifres</h2>
+            
+        </div>
+    )
+}
+
+export default FartishionEnChiffre
