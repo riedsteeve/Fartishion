53dcd91c61588ef03b244d09139a4b9e6e76af08
Steeven HOUNKPE-SAGBO <e.hounkpesagbo@ecoles-epsi.net>
2026-06-22 15:28:06 +0200

    Mise en place de la section statistics

--- frontend/src/Components/Composables/fartishionEnChiffre.tsx
+++ frontend/src/Components/Composables/fartishionEnChiffre.tsx
@@ -8,9 +8,23 @@ const stats = [
 
 const FartishionEnChiffre = () => {
     return (
-        <div className="mt-20 bg-purple-600 rounded-4xl p-7">
+        <div className="mt-20 rounded-4xl p-7">
 
             <h2 className="text-3xl text-center font-pt-serif" >Fartshion en chifres</h2>
+
+            <p className="text-center mt-7 text-gray-600" >Une communauté musicale en pleine croissance, portée <br /> par la passion du partage.</p>
+
+            <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10 mt-8 px-20">
+                {stats.map((stat) => (
+                    <div
+                        className="bg-purple-100 text-center border border-purple-800 p-8 rounded-3xl w-64 m-auto"
+                        key={stat.id}
+                    >
+                        <h4 className="text-3xl text-purple-800 font-bold" >{stat.chiffre}</h4>
+                        <p className="text-purple-950" >{ stat.description }</p>
+                    </div>
+                ))}
+            </div>
             
         </div>
     )
